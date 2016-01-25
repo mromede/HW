6761f8aba132f9285e9346b8630397571ed34101
Israel O <mromede@gmail.com>
2016-01-25 05:05:07 -0500

    New Clock Update

---
+++ diff.js
@@ -0,0 +1,24 @@
+// $(document).ready(function(){
+
+// 	function TheClock(){
+// 		var d, h, m, s;
+// 		d = 5;
+// 		h=6;
+// 		console.log(d);
+// 		console.log(h)
+// 	}
+
+// }); 
+
+function TheClock(){
+	setInterval(function(){
+		var d, h, m, s;
+		d = new Date();
+		h = d.getHours();
+		m = d.getMinutes();
+		s = d.getSeconds();
+		$(".sec").text(s);
+		$(".min").text(m);
+		$(".hour").text(h);
+  	},100)
+} 
